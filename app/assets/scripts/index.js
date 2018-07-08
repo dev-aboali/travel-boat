ae690ea87bf95374bdc95fd2af5911d1690d0cca
Ahmed <dev.ahmed93@gmail.com>
2018-07-08 21:20:03 +0200

    Completed the reveal on scroll

--- app/assets/scripts/index.js
+++ app/assets/scripts/index.js
@@ -1,4 +1,6 @@
-import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
-
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 const mobile = new MobileMenu();
+new RevealOnScroll($('.feature-item'), "85%");
+new RevealOnScroll($('.testimonial') , "60%");
