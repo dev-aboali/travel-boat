2fa2628d19ec67fd317135d337f95240d7f9336b
Ahmed <dev.ahmed93@gmail.com>
2018-07-10 00:48:29 +0200

    Completed sticky header

--- app/assets/scripts/index.js
+++ app/assets/scripts/index.js
@@ -1,6 +1,8 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
 const mobile = new MobileMenu();
 new RevealOnScroll($('.feature-item'), "85%");
 new RevealOnScroll($('.testimonial') , "60%");
+var stickyHeader = new StickyHeader();
